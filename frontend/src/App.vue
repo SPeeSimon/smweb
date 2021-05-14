8c84fe73c104b179ad6f7aa9e805ac2ffbf2c476
simon haverdings <shaverdings@hotmail.com>
2021-05-14 15:41:42 +0200

    Fix styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -89,7 +89,7 @@ export default class Home extends Vue {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
 }
 
