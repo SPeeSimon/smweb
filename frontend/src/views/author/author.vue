60e9f75717a9869514725a30a739664eadcdc26a
simon haverdings <shaverdings@hotmail.com>
2021-05-15 17:04:09 +0200

    Author page

--- frontend/src/views/author/author.vue
+++ frontend/src/views/author/author.vue
@@ -1,33 +1,53 @@
 <template>
-  <div class="well" v-if="author">
-    <div class="panel panel-default">
-      <div class="panel-heading"><span>Author #</span><span v-text="author.id"></span></div>
-      <div class="panel-body">
-        <div class="form-horizontal">
-          <div class="input-group">
-            <span class="input-group-addon" style="width: 8em">Name</span>
-            <input
-              type="text"
-              class="form-control"
-              placeholder="How (s)he is called"
-              :value="author.name"
-              :readonly="cantWrite"
-            />
+  <div v-if="author">
+    <div class="well">
+      <div class="card card-default">
+        <div class="card-header"><span>Author #</span><span v-text="author.id"></span></div>
+        <div class="card-body">
+          <div class="row py-2">
+            <div class="form-horizontal">
+              <div class="input-group">
+                <span class="input-group-addon" style="width: 8em">Name</span>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="How (s)he is called"
+                  :value="author.name"
+                  :readonly="cantWrite"
+                />
+              </div>
+              <div class="input-group">
+                <span class="input-group-addon" style="width: 8em">Notes</span>
+                <textarea
+                  class="form-control"
+                  rows="3"
+                  :value="author.notes"
+                  :readonly="cantWrite"
+                />
+              </div>
+            </div>
           </div>
         </div>
-        <div class="row">
-          <model-short v-for="model in models" :key="model.id" :model="model" />
-        </div>
       </div>
-      <div class="panel-footer">
-        <span></span>
+    </div>
+    <div class="well py-5">
+      <div class="card card-default">
+        <div class="card-header">
+          <span>Models created by {{author.name}}</span>
+          <span class="btn pull-right fa fa-sync" @click="reload" title="Refresh"></span>
+        </div>
+        <div class="card-body">
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-3">
+            <model-short v-for="model in models" :key="model.id" :model="model"></model-short>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Watch } from "vue-property-decorator";
 import ReloadButton from "../../components/ReloadButton.vue";
 import { AuthorInfo, AuthorService } from "../../services/AuthorService";
 import { ModelService } from "../../services/ModelService";
@@ -35,40 +55,37 @@ import ModelShort from "../model/model-short.vue";
 
 @Component({
   components: {
-    ReloadButton, ModelShort
+    ReloadButton,
+    ModelShort,
   },
 })
 export default class extends Vue {
-  protected author: AuthorInfo;
+  protected author: AuthorInfo = null;
   protected models: any[] = [];
   protected cantWrite = true;
 
-  created() {
-    this.reload();
-  }
-
+  @Watch("$route.params", { immediate: true, deep: true })
   public reload() {
     this.getAuthor();
     this.getAuthorModels();
   }
 
   getAuthor() {
-    new AuthorService("").get(this.$router.id)
-      .then((data) => {
-        this.author = data;
-      });
+    new AuthorService("").get(this.$route.params.id).then((data) => {
+      this.author = data;
+    });
   }
 
   getAuthorModels() {
     // TODO add pagination for models
-    new ModelService('').getByAuthor(this.$router.id)
-      .then(data => {
+    new ModelService("")
+      .getByAuthor(this.$route.params.id)
+      .then((data) => {
         this.models = Array.from(data);
       })
-      .catch(function(err){
+      .catch(function (err) {
         //TODO: notify user
-      })
+      });
   }
-
 }
 </script>
